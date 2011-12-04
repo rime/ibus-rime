bdfbe2108ed68d8d5fc75f04364e7a6635dd66e9
lotem <chen.sst@gmail.com>
2011-12-04 13:20:18 +0000

    Set ibus-rime distribution info.
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/ibus-rime@373 b90aed45-4297-9963-ff9d-98b604ac09a9

--- rime_main.c
+++ rime_main.c
@@ -9,19 +9,20 @@
 #include <rime_api.h>
 #include "rime_engine.h"
 
+// TODO:
+#define _(x) (x)
+
+#define DISTRIBUTION_NAME _("Rime")
+#define DISTRIBUTION_CODE_NAME "ibus-rime"
+#define DISTRIBUTION_VERSION "0.9"
+
 #define IBUS_RIME_INSTALL_PREFIX "/usr"
+#define IBUS_RIME_SHARED_DATA_DIR IBUS_RIME_INSTALL_PREFIX "/share/ibus-rime"
 
-static const char* ibus_rime_shared_data_dir() {
-  return IBUS_RIME_INSTALL_PREFIX "/share/ibus-rime";
-}
-
-static const char* ibus_rime_user_data_dir() {
+static const char* get_ibus_rime_user_data_dir(char *path) {
   const char* home = getenv("HOME");
-  static char path[1024] = "";
-  if (!path[0]) {
-    strcpy(path, home);
-    strcat(path, "/.ibus/rime");
-  }
+  strcpy(path, home);
+  strcat(path, "/.ibus/rime");
   return path;
 }
 
@@ -40,11 +41,16 @@ static void rime_with_ibus() {
     exit(0);
   }
 
-  g_mkdir_with_parents(ibus_rime_user_data_dir(), 0700);
+  char user_data_dir[1024] = "";
+  get_ibus_rime_user_data_dir(user_data_dir);
+  g_mkdir_with_parents(user_data_dir, 0700);
 
   RimeTraits ibus_rime_traits;
-  ibus_rime_traits.shared_data_dir = ibus_rime_shared_data_dir();
-  ibus_rime_traits.user_data_dir = ibus_rime_user_data_dir();
+  ibus_rime_traits.shared_data_dir = IBUS_RIME_SHARED_DATA_DIR;
+  ibus_rime_traits.user_data_dir = user_data_dir;
+  ibus_rime_traits.distribution_name = DISTRIBUTION_NAME;
+  ibus_rime_traits.distribution_code_name = DISTRIBUTION_CODE_NAME;
+  ibus_rime_traits.distribution_version = DISTRIBUTION_VERSION;
   RimeInitialize(&ibus_rime_traits);
 
   g_signal_connect(bus, "disconnected", G_CALLBACK(ibus_disconnect_cb), NULL);
