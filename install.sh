2e7f1b3cd8b6adbb7239f4b18f40f0870da613d8
lotem <chen.sst@gmail.com>
2013-01-11 10:54:21 +0800

    no comment.

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-(cd ../librime; make && sudo make install) || exit 1
-(cd ../brise; make && sudo make install) || exit 1
+if [ ! -e librime -a -e ../librime ]; then ln -s ../librime; fi
+if [ ! -e brise -a -e ../brise ]; then ln -s ../brise; fi
+
+(cd librime; make && sudo make install) || exit 1
+(cd brise; make && sudo make install) || exit 1
+
 make clean && make && sudo make install && ibus-daemon -drx
